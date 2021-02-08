af0c49873c13ee50eb1ac30447ad755ab81bfafe
DESKTOP-AVTI26A\burto <matthewburton@webster.edu>
2021-02-08 17:18:49 -0600

    Session 1 FINISHED. Player movement and shooting

---
+++ Space Shooter Unity/Assets/Code/PlayerShip.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerShip : Ship
+{
+    void Update()
+    {
+        FollowMouse();
+        HandleUserInput();
+    }
+
+    void HandleUserInput()
+    {
+        if (Input.GetMouseButton(1))
+        {
+            Thrust();
+        }
+
+        if (Input.GetMouseButtonDown(0))
+        {
+            FireProjectile();
+        }
+    }
+
+    void FollowMouse()
+    {
+        Vector3 mousePosition = Camera.main.ScreenToWorldPoint(Input.mousePosition); //Finds Mouse Position on Screen
+        Vector2 directionToFace = new Vector2(mousePosition.x - transform.position.x, mousePosition.y - transform.position.y); //creates direction based on positon of ship and mouse cursor 
+        transform.up = directionToFace; //Faces Mouse. Assigns transform.up the Direction to Face
+    }
+}
---
+++ Space Shooter Unity/Assets/Code/Ship.cs
@@ -0,0 +1,51 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public abstract class Ship : MonoBehaviour
+{
+    public Rigidbody2D rigidBody2D;
+    public GameObject projectilePrefab;
+    public Transform projectileSpawnPoint;
+
+    public float acceleration;
+    public float maxSpeed;
+    public int maxArmor;
+    public float fireRate;
+    public float projectileSpeed;
+
+    [HideInInspector] public float currentSpeed;
+    [HideInInspector] public int currentArmor;
+
+    void FixedUpdate()
+    {
+        if (rigidBody2D.velocity.magnitude > maxSpeed)
+        {
+            rigidBody2D.velocity = rigidBody2D.velocity.normalized * maxSpeed;
+        }
+    }
+
+    public void Thrust()
+    {
+        rigidBody2D.AddForce(transform.up * acceleration); //Add force in the direction we're facing
+        currentSpeed = maxSpeed; //Set our speed to our max speed
+        //TODO: Create particle effects
+    }
+
+    public void FireProjectile()
+    {
+        GameObject projectile = Instantiate(projectilePrefab, projectileSpawnPoint.position, transform.rotation);
+        projectile.GetComponent<Rigidbody2D>().AddForce(transform.up * projectileSpeed);
+        Destroy(projectile, 4);
+    }
+
+    public void TakeDamage()
+    {
+
+    }
+
+    public void Explode()
+    {
+
+    }
+}
