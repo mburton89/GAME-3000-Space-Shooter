55d4415874768d9aadbbedee4f2f2f1d60043655
WEBSTER UNIVERSITY <DTS@WEBSTER.EDU>
2021-04-30 17:49:16 -0500

    im done for the day

--- Space Shooter Unity/Assets/Code/Array.cs
+++ Space Shooter Unity/Assets/Code/Array.cs
@@ -4,12 +4,30 @@ using UnityEngine;
 
 public class Array : MonoBehaviour
 {
-
+    int totalPlayers = 1;
+    public int currentplayerIndex;
     public GameObject[] players;
+    public GameObject currentPlayer;
+    public GameObject playerHolder;
 
     // Start is called before the first frame update
     void Start()
     {
+        //totalPlayers = playerHolder.transform.childCount;
+        //players = new GameObject[totalPlayers];
+
+        //for (int i = 0; i < totalPlayers; i++)
+        //{
+
+
+
+
+        //}
+
+
+        
+
+
 
     }
 
@@ -25,8 +43,9 @@ public class Array : MonoBehaviour
     public void chooseRandomShip()
     {
         int rand = Random.Range(0, players.Length);
-
+       
         GameObject newship = players[rand];
+        
 
     }
 
