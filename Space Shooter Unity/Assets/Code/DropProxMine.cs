e61edad3a5bb85884607d24469ebd04925b2748a
DESKTOP-AVTI26A\burto <matthewburton@webster.edu>
2021-05-13 15:46:52 -0500

    mines

--- Space Shooter Unity/Assets/Code/DropProxMine.cs
+++ Space Shooter Unity/Assets/Code/DropProxMine.cs
@@ -8,52 +8,59 @@ using TMPro;
 
 public class DropProxMine : MonoBehaviour
 {
-    public GameObject proxMinePrefab;
-    public float mineTriggerDistance;
+    public Projectile proxMinePrefab;
     public int minesLeftCounter;
+    public int mineWaitTime;
     public Transform mineSpawnPoint;
+    public bool canDrop;
+    private bool canResetMines;
 
     public TextMeshProUGUI mineWarningText;
 
-
-    public void DropMine()
+    
+    private void Awake()
     {
-        if (Input.GetButtonDown("space"))
+        canDrop = true;
+        canResetMines = true;
+        mineWarningText.SetText("Mines left: " + minesLeftCounter);
+        
+    }
+
+    public void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Space) && canDrop == true)
         {
-            GameObject proxMine = Instantiate(proxMinePrefab, mineSpawnPoint.position, transform.rotation)as GameObject;
+            Projectile proxMine = Instantiate(proxMinePrefab, mineSpawnPoint.position, transform.rotation)as Projectile;
+            proxMine.Init(GetComponent<PlayerShip>().gameObject);
+
+            Vector3 shipVelocity = new Vector3();
+            shipVelocity = FindObjectOfType<PlayerShip>().rigidBody2D.velocity;
+            proxMine.rigidBody2D.velocity = shipVelocity / 3;
+
+            minesLeftCounter--;
+            mineWarningText.SetText("Mines left: " + minesLeftCounter);
         }
 
-        if(minesLeftCounter == 0)
+        if (minesLeftCounter == 0 && canResetMines)
         {
+            canResetMines = false;
+            StartCoroutine(MineDropWait());
             mineWarningText.SetText("OUT OF MINES! RESTOCK!");
         }
-        else
-            minesLeftCounter--;
     }
 
-
-    public void MineTrigger()
+    private IEnumerator MineDropWait()
     {
+        canDrop = false;
+        yield return new WaitForSeconds(mineWaitTime);
+        canDrop = true;
+        minesLeftCounter = 5;
 
-        Explode();
-    }
-    
-
-
-
-    public void Explode()
-    {
-        Instantiate(Resources.Load("ShipExplosion"), transform.position, transform.rotation);
-        //ScreenShaker.Instance.ShakeScreen();
-        ScreenShakeManager.Instance.ShakeScreen();
-        EnemyShipSpawner.Instance.CountEnemyShips();
-
-        if (GetComponent<PlayerShip>())
+        if (minesLeftCounter > 0)
         {
-            GameManager.Instance.HandlePlayerDestroyed();
+            mineWarningText.SetText("Mines left: " + minesLeftCounter);
         }
 
-        Destroy(gameObject);
+        canResetMines = true;
     }
-
 }
