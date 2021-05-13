eed6a3e7116ae82f2bed2bbbd6a07afd0661f584
DESKTOP-AVTI26A\burto <matthewburton@webster.edu>
2021-05-13 15:56:03 -0500

    mines and mace

--- Space Shooter Unity/Assets/Code/DropProxMine.cs
+++ Space Shooter Unity/Assets/Code/DropProxMine.cs
@@ -6,47 +6,61 @@ using UnityEngine.SceneManagement;
 using TMPro;
 
 
-public class DropProxMine : Ship
+public class DropProxMine : MonoBehaviour
 {
     public Projectile proxMinePrefab;
     public int minesLeftCounter;
+    public int mineWaitTime;
     public Transform mineSpawnPoint;
-    public AudioSource explodeSound;
-    public Animation explodeAnimation;
+    public bool canDrop;
+    private bool canResetMines;
 
     public TextMeshProUGUI mineWarningText;
 
+    
+    private void Awake()
+    {
+        canDrop = true;
+        canResetMines = true;
+        mineWarningText.SetText("Mines left: " + minesLeftCounter);
+        
+    }
 
     public void Update()
     {
-        if (Input.GetKeyDown(KeyCode.Space))
+        if (Input.GetKeyDown(KeyCode.Space) && canDrop == true)
         {
             Projectile proxMine = Instantiate(proxMinePrefab, mineSpawnPoint.position, transform.rotation)as Projectile;
             proxMine.Init(GetComponent<PlayerShip>().gameObject);
-        }
 
-        if (minesLeftCounter == 0)
-        {
-            mineWarningText.SetText("OUT OF MINES! RESTOCK!");
-        }
-        else
-        {
+            Vector3 shipVelocity = new Vector3();
+            shipVelocity = FindObjectOfType<PlayerShip>().rigidBody2D.velocity;
+            proxMine.rigidBody2D.velocity = shipVelocity / 3;
+
             minesLeftCounter--;
             mineWarningText.SetText("Mines left: " + minesLeftCounter);
         }
 
-    }
-
-
-    public void MineTrigger(Collision2D collision)
-    {
-        if (collision.gameObject.GetComponent<EnemyShip>())
+        if (minesLeftCounter == 0 && canResetMines)
         {
-            explodeSound.Play();
-            explodeAnimation.Play();
-            Explode();
-            collision.gameObject.GetComponent<EnemyShip>().TakeDamage(1);
+            canResetMines = false;
+            StartCoroutine(MineDropWait());
+            mineWarningText.SetText("OUT OF MINES! RESTOCK!");
         }
     }
 
+    private IEnumerator MineDropWait()
+    {
+        canDrop = false;
+        yield return new WaitForSeconds(mineWaitTime);
+        canDrop = true;
+        minesLeftCounter = 5;
+
+        if (minesLeftCounter > 0)
+        {
+            mineWarningText.SetText("Mines left: " + minesLeftCounter);
+        }
+
+        canResetMines = true;
+    }
 }
